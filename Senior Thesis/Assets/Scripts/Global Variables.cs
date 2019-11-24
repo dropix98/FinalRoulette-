c3716938953bccbc6d70a8ef791a7f211f066f51
Justin <bindi@chapman.edu>
2019-11-24 02:18:26 -0800

    Script Additions
    Added 2 scripts. One for global variables and one for interaction with 2D colliders. There is now a global variable that counts the amount of key items you find. You can now also press "E" to interact with the items. Simple 2D colliders on OfficeWalls 1 and Character for now as a test; if you press "E" while character is overlapping the wall collider, the global counting variable goes up by one (also there is a check to make sure you can only increment the variable one time per item).

---
+++ Senior Thesis/Assets/Scripts/Global Variables.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalVariables
+{
+    public static float itemsFound;
+    public static bool inWallCollider;
+    public static bool foundWallColliderItem;
+
+
+}
---
+++ Senior Thesis/Assets/Scripts/Interact.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      GlobalVariables.itemsFound = 0.0f;
+      Debug.Log("Number of Items Found: " + GlobalVariables.itemsFound);
+      GlobalVariables.inWallCollider = false;
+      GlobalVariables.foundWallColliderItem = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //'e' is interact key
+      if (Input.GetKeyDown("e"))
+      {
+          print("E key was pressed");
+
+          //checking if in collider and haven't pressed 'e' with it before;
+          //eventually will have checks for all key items to interact with;
+          //also will have a similar check that checks if itemsFound variable
+          ////is at proper value to allow completion of level when in that collider;
+          if (GlobalVariables.inWallCollider == true && GlobalVariables.foundWallColliderItem == false)
+          {
+            Debug.Log("E key was pressed in wallCollider");
+            GlobalVariables.itemsFound++;
+            GlobalVariables.foundWallColliderItem = true;
+            Debug.Log("Number of Items Found: " + GlobalVariables.itemsFound);
+          }
+      }
+    }
+
+    //checks if in a collider or not;
+    //eventually will have name checks for all key items to interact with;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "OfficeWalls 1")
+        {
+          Debug.Log("Colliding with Wall");
+          GlobalVariables.inWallCollider = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "OfficeWalls 1")
+        {
+          Debug.Log("Not Colliding with Wall Anymore");
+          GlobalVariables.inWallCollider = false;
+        }
+    }
+  }
