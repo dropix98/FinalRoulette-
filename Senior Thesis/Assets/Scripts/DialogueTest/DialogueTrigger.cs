33c66ec2a41eb54b6b80eb8a90ebb839a7e627c2
Carina Chao <dropix98@gmail.com>
2019-12-12 11:28:13 -0800

    Dialogue Into UI Tests Done
    Have the scripts for Dialogue to be done in UI.

---
+++ Senior Thesis/Assets/Scripts/DialogueTest/DialogueTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Character")
+        {
+            Debug.Log("Player Detected");
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                TriggerDialogue();
+            }
+            
+        }
+    }
+}
