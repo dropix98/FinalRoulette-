b9b4d90d24b3c552b9eda69cb922a3aae8d408f2
Justin <bindi@chapman.edu>
2020-02-14 08:42:19 -0800

    fix trigger code comment

--- Senior Thesis/Assets/Scripts/DialogueTest/DialogueTrigger.cs
+++ Senior Thesis/Assets/Scripts/DialogueTest/DialogueTrigger.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
-    
+
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
 
     }
 
-    //void OnTriggerStay2D(Collider2D other)
-    //{
-    //    if (other.gameObject.name == "Character")
-    //    {
-    //        Debug.Log("Player Detected");
-    //        if (Input.GetKeyDown(KeyCode.Space))
-    //        {
-    //            TriggerDialogue();
-    //        }
-            
-    //    }
-    //}
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Character")
+        {
+            Debug.Log("Player Detected");
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                TriggerDialogue();
+            }
+
+        }
+    }
 }
