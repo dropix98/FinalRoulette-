449f16f855e529b16afed6a2f38e7770023e84e6
Justin <bindi@chapman.edu>
2020-02-19 10:00:51 -0800

    Update BlockPuzzle.cs

--- Senior Thesis/Assets/Scripts/PuzzleScripts/BlockPuzzle.cs
+++ Senior Thesis/Assets/Scripts/PuzzleScripts/BlockPuzzle.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BlockPuzzle : MonoBehaviour
 {
     string objectName = "";
+    float localRotation = -1;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,20 +15,23 @@ public class BlockPuzzle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      //if ()
+      if (localRotation != -1)
+      {
 
-      if (Input.GetKeyDown("d"))
-        {
-            print("Turn Right key was pressed");
-        }
-      if (Input.GetKeyDown("a"))
-        {
-            print("Turn Left key was pressed");
-        }
+        if (Input.GetKeyDown("d"))
+          {
+              print("Turn Right key was pressed");
+          }
+        if (Input.GetKeyDown("a"))
+          {
+              print("Turn Left key was pressed");
+          }
+       }
     }
 
     void OnMouseDown()
     {
+      localRotation = this.transform.rotation.eulerAngles.y;
       objectName = this.gameObject.name;
       Debug.Log(objectName);
     }
