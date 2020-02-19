90b8483bc2152f27f4b2db01aa9759824cb184d2
Justin <bindi@chapman.edu>
2020-02-19 09:33:38 -0800

    block puzzle progress

---
+++ Senior Thesis/Assets/Scripts/PuzzleScripts/BlockPuzzle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPuzzle : MonoBehaviour
+{
+    string objectName = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    void OnMouseDown()
+    {
+      objectName = this.gameObject.name;
+      Debug.Log(objectName);
+    }
+}
