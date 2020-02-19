b8c169a27afdc655d956885f66cf9ebe2343ac30
Justin <bindi@chapman.edu>
2020-02-19 09:48:35 -0800

    Update BlockPuzzle.cs

--- Senior Thesis/Assets/Scripts/PuzzleScripts/BlockPuzzle.cs
+++ Senior Thesis/Assets/Scripts/PuzzleScripts/BlockPuzzle.cs
@@ -14,7 +14,16 @@ public class BlockPuzzle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
+      //if ()
+
+      if (Input.GetKeyDown("d"))
+        {
+            print("Turn Right key was pressed");
+        }
+      if (Input.GetKeyDown("a"))
+        {
+            print("Turn Left key was pressed");
+        }
     }
 
     void OnMouseDown()
