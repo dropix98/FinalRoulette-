41caed58705c67ef7c3a2463eea1a035f4cdb07a
Justin <bindi@chapman.edu>
2020-02-19 12:35:04 -0800

    block progress

--- Senior Thesis/Assets/Scripts/PuzzleScripts/BlockPuzzle.cs
+++ Senior Thesis/Assets/Scripts/PuzzleScripts/BlockPuzzle.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class BlockPuzzle : MonoBehaviour
 {
     string objectName = "";
-    float localRotation = -1;
+    /*public gameObject block1;
+    public gameObject block2;
+    public gameObject block3;
+    public gameObject block4;*/
+    float localRotation;
+    //public float RotationSpeed = 2.0f;
+    //Vector3 rightturn = new Vector3(0f, 90f, 0f);
+    float rotationAmount = 75.000f;
+    //private float timer = 0.0f;
+    //private float waitTime = 1.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+      localRotation = -1;
     }
 
     // Update is called once per frame
@@ -17,14 +28,24 @@ public class BlockPuzzle : MonoBehaviour
     {
       if (localRotation != -1)
       {
-
+        //while (timer <= waitTime)// (localRotation == 90 || localRotation == 180 || localRotation == 270 || localRotation == 360)
+        //{
+          transform.Rotate (new Vector3(0, rotationAmount, 0) * Time.deltaTime);
+          //timer += Time.deltaTime;
+          //print("timer");
+        //}
         if (Input.GetKeyDown("d"))
           {
               print("Turn Right key was pressed");
+              transform.Rotate(0, 90.000f, 0);
+
+              //transform.Rotate ( rightturn * ( RotationSpeed * Time.deltaTime ) );
+              //objectName.transform.Rotate (new Vector3 (0, 90, 0) * Time.deltaTime);
           }
         if (Input.GetKeyDown("a"))
           {
               print("Turn Left key was pressed");
+
           }
        }
     }
