64ab5f25190bc4e867d45cce549da827988dd37d
Carina Chao <dropix98@gmail.com>
2020-02-27 14:09:15 -0800

    ScriptableObjectsBasis
    Impressions Scriptable Objects can be set up and can be displayed on UI

---
+++ Senior Thesis/Assets/ImpressionDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class ImpressionDisplay : MonoBehaviour
+{
+    public Impression impression;
+
+    public Text nameCharacter;
+    public Text impressionShort;
+    public Text impressionDescription;  
+    void Start()
+    {
+        nameCharacter.text = impression.nameOfCharacter;
+        impressionShort.text = impression.impressionShort;
+        impressionDescription.text = impression.impressionDescription; 
+    }
+
+    
+}
---
+++ Senior Thesis/Assets/Scripts/Inventory/Impression.cs
@@ -0,0 +1,12 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Impression", menuName ="Impression")]
+public class Impression : ScriptableObject
+{
+    public string nameOfCharacter;
+    public string impressionShort;
+    public string impressionDescription; 
+
+}
