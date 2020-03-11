37e2b0fc39c45689e2d71372e3b40571b528ed9d
Carina Chao <dropix98@gmail.com>
2020-03-11 08:36:39 -0700

    Inventory Scriptable Object
    Inventory Scriptable Object Class and Display in

---
+++ Senior Thesis/Assets/Scripts/Inventory/Inventory/Inventory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory")]
+public class Inventory : ScriptableObject
+{
+    public string nameOfCharacter;
+    public string inventoryShort;
+    public string inventoryDescription;
+}
---
+++ Senior Thesis/Assets/Scripts/Inventory/Inventory/InventoryDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryDisplay : MonoBehaviour
+{
+    public Inventory inventory;
+
+    public Text nameCharacter;
+    public Text inventoryShort;
+    public Text inventoryDescription;
+    void Update()
+    {
+        nameCharacter.text = inventory.nameOfCharacter;
+        inventoryShort.text = inventory.inventoryShort;
+        inventoryDescription.text = inventory.inventoryDescription;
+    }
+}
