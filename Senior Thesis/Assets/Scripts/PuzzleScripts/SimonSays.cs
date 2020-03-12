0c48c461c837d73355e22799b02dcb5dccbc3dfa
Justin <bindi@chapman.edu>
2020-03-12 16:55:12 -0700

    simon says- stuck

--- Senior Thesis/Assets/Scripts/PuzzleScripts/SimonSays.cs
+++ Senior Thesis/Assets/Scripts/PuzzleScripts/SimonSays.cs
@@ -1,27 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SimonSays : MonoBehaviour
 {
     char[] colors = new char[7];
-    bool level1 = false;
-    bool level2 = false;
-    bool level3 = false;
-    bool level4 = false;
-    bool level5 = false;
-    bool level6 = false;
-    bool level7 = false;
+
 
     public GameObject blue;
     public GameObject pink;
     public GameObject green;
     public GameObject yellow;
-
+    string concat;
 
     public float delay = 5F;
     private float remainingTime;
 
+    public bool Check2 = true;
+
+    bool level1;
+    bool level2;
+    bool level2_;
+    bool level3;
+    bool level3_;
+    bool level3__;
+    bool level4;
+    bool level5;
+    bool level6;
+    bool level7;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +39,19 @@ public class SimonSays : MonoBehaviour
       pink.SetActive(false);
 
       remainingTime = delay;
+
+      level1 = false;
+      level2 = false;
+      level2_ = false;
+      level3 = false;
+      level4 = false;
+      level5 = false;
+      level6 = false;
+      level7 = false;
+      for ( int i = 0; i < colors.Length; i++)
+      {
+        colors[i] = 'z';
+      }
 
     }
 
@@ -49,9 +69,91 @@ public class SimonSays : MonoBehaviour
         {
          //remainingTime = delay;
          blue.SetActive(false);
+         Level1Check();
+        }
+      }
+
+      if (level2)
+      {
+        if(remainingTime > 0)
+        {
+          blue.SetActive(true);
+          remainingTime -= Time.deltaTime;
+        }
+        else
+        {
+          blue.SetActive(false);
+          level2 = false;
+          level2_ = true;
+          remainingTime = delay;
+        }
+      }
+
+        if (level2_)
+        {
+          if(remainingTime > 0)
+          {
+            pink.SetActive(true);
+            remainingTime -= Time.deltaTime;
+          }
+          else
+          {
+            pink.SetActive(false);
+            //print("hi");
+            //if (Check2)
+            //{
+              Level2Check();
+              print ("hey");
+
+              //Check2 = false;
+            //}
+          }
         }
 
-      }
+        if (level3)
+        {
+          if(remainingTime > 0)
+          {
+            blue.SetActive(true);
+            remainingTime -= Time.deltaTime;
+          }
+          else
+          {
+            blue.SetActive(false);
+            level3 = false;
+            level3_ = true;
+          }
+        }
+        if (level3_)
+        {
+          if(remainingTime > 0)
+          {
+            pink.SetActive(true);
+            remainingTime -= Time.deltaTime;
+          }
+          else
+          {
+            pink.SetActive(false);
+            level3_ = false;
+            level3__ = true;
+          }
+        }
+        if (level3__)
+        {
+          if(remainingTime > 0)
+          {
+            blue.SetActive(true);
+            remainingTime -= Time.deltaTime;
+          }
+          else
+          {
+            blue.SetActive(false);
+            level3__ = false;
+            //Level3Check();
+          }
+        }
+
+
     }
 
     public void startGame()
@@ -61,55 +163,154 @@ public class SimonSays : MonoBehaviour
 
     public void Level1Check()
     {
-      
+      if (colors[0] == 'b')
+      {
+        level1 = false;
+        level2 = true;
+        remainingTime = delay;
+      }
+      else if (colors[0] == 'y' || colors[6] == 'p' || colors[6] == 'g')
+      {
+        FailGame();
+      }
+    }
+
+    public void Level2Check()
+    {
+        for ( int i = 0; i < colors.Length; i++)
+        {
+          colors[i] = 'z';
+        }
+        print ("start");
+
+        if (colors[1] == 'p' && colors[0] == 'b')
+        {
+          level2_ = false;
+          level3 = true;
+          remainingTime = delay;
+          print ("hello");
+        }
+        else if (colors[1] == 'y' || colors[1] == 'b' || colors[1] == 'g' ||
+                 colors[0] == 'y' || colors[0] == 'p' || colors[0] == 'g')
+        {
+          FailGame();
+        }
+
+    }
+
+    public void FailGame()
+    {
+      SceneManager.LoadScene("Lounge");
     }
 
     public void setBlue()
     {
       print("b");
-      colors[0] = colors[1];
+      for ( int i = 0; i < colors.Length; i++)
+      {
+        if (colors[i] == 'z')
+        {
+          colors[i] = 'b';
+          break;
+        }
+      }
+
+      concat = "";
+      for ( int i = 0; i < colors.Length; i++)
+      {
+         concat = concat + colors[i];
+      }
+      print (concat);
+      /*colors[0] = colors[1];
       colors[1] = colors[2];
       colors[2] = colors[3];
       colors[3] = colors[4];
       colors[4] = colors[5];
       colors[5] = colors[6];
-      colors[6] = 'b';
+      colors[6] = 'b';*/
 
     }
     public void setPink()
     {
       Debug.Log("p");
-      colors[0] = colors[1];
+      for ( int i = 0; i < colors.Length; i++)
+      {
+        if (colors[i] == 'z')
+        {
+          colors[i] = 'p';
+          break;
+        }
+      }
+
+      concat = "";
+
+      for ( int i = 0; i < colors.Length; i++)
+      {
+         concat = concat + colors[i];
+      }
+      print (concat);
+      /*colors[0] = colors[1];
       colors[1] = colors[2];
       colors[2] = colors[3];
       colors[3] = colors[4];
       colors[4] = colors[5];
       colors[5] = colors[6];
-      colors[6] = 'r';
+      colors[6] = 'p';*/
 
     }
     public void setYellow()
     {
       Debug.Log("y");
-      colors[0] = colors[1];
+      for ( int i = 0; i < colors.Length; i++)
+      {
+        if (colors[i] == 'z')
+        {
+          colors[i] = 'y';
+          break;
+        }
+      }
+      concat = "";
+
+      for ( int i = 0; i < colors.Length; i++)
+      {
+         concat = concat + colors[i];
+      }
+      print (concat);
+      /*colors[0] = colors[1];
       colors[1] = colors[2];
       colors[2] = colors[3];
       colors[3] = colors[4];
       colors[4] = colors[5];
       colors[5] = colors[6];
-      colors[6] = 'g';
+      colors[6] = 'y';*/
 
     }
     public void setGreen()
     {
       Debug.Log("g");
-      colors[0] = colors[1];
+      for ( int i = 0; i < colors.Length; i++)
+      {
+        if (colors[i] == 'z')
+        {
+          colors[i] = 'g';
+          break;
+        }
+      }
+      concat = "";
+
+
+      for ( int i = 0; i < colors.Length; i++)
+      {
+         concat = concat + colors[i];
+      }
+      print (concat);
+      /*colors[0] = colors[1];
       colors[1] = colors[2];
       colors[2] = colors[3];
       colors[3] = colors[4];
       colors[4] = colors[5];
       colors[5] = colors[6];
-      colors[6] = 'g';
+      colors[6] = 'g';*/
 
     }
 }
