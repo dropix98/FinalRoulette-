aae932822382b2ccc1461d96afc3aa1735d75a49
kyleburack <burack@chapman.edu>
2020-03-12 23:44:14 -0700

    added free flow updated

--- Senior Thesis/Assets/Scripts/PuzzleScripts/FreeFlowScript.cs
+++ Senior Thesis/Assets/Scripts/PuzzleScripts/FreeFlowScript.cs
@@ -4,15 +4,87 @@ using UnityEngine;
 
 public class FreeFlowScript : MonoBehaviour
 {
+
+    GameObject temp;
+    public Material orange;
+    public Material pink;
+    public Material darkblue;
+    public Material purple;
+    public Material green;
+    public Material teal;
+    public Material yellow;
+    public Material lightblue;
+    public Material red;
+    public Material lilac;
+
+    public Material newMaterial;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
+    public void OnMouseDown(GameObject go)
+    {
+        temp = go;
+
+        go.GetComponent<MeshRenderer>().material = newMaterial;
+    }
+
     void Update()
     {
-        
+        if (temp == GameObject.Find("Orange1") || temp == GameObject.Find("Orange2"))
+        {
+            //if stataement to change the material on the game object and to check if the mouse is still down.
+            //going to make a temp one in another function that is on click to change the color
+            newMaterial = orange;
+        }
+
+        if (temp == GameObject.Find("Pink1") || temp == GameObject.Find("Pink2"))
+        {
+            newMaterial = pink;
+        }
+
+        if (temp == GameObject.Find("DarkBlue1") || temp == GameObject.Find("DarkBle2"))
+        {
+            newMaterial = darkblue;
+        }
+
+        if (temp == GameObject.Find("Purple1") || temp == GameObject.Find("Purple2"))
+        {
+            newMaterial = purple;
+        }
+
+        if (temp == GameObject.Find("Green1") || temp == GameObject.Find("Green2"))
+        {
+            newMaterial = green;
+        }
+
+        if (temp == GameObject.Find("Teal1") || temp == GameObject.Find("Teal2"))
+        {
+            newMaterial = teal;
+        }
+
+        if (temp == GameObject.Find("Yellow1") || temp == GameObject.Find("Yellow2"))
+        {
+            newMaterial = yellow;
+        }
+
+        if (temp == GameObject.Find("LightBlue1") || temp == GameObject.Find("LightBlue2"))
+        {
+            newMaterial = lightblue;
+        }
+
+        if (temp == GameObject.Find("Red1") || temp == GameObject.Find("Red2"))
+        {
+            newMaterial = red;
+        }
+
+        if (temp == GameObject.Find("Lilac1") || temp == GameObject.Find("Lilac2"))
+        {
+            newMaterial = lilac;
+        }
     }
 }
