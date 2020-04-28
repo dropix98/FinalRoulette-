c129288ae71848aecf6369d39a66c7a0e1e08f39
kyleburack <42784552+kyleburack@users.noreply.github.com>
2020-04-27 20:46:55 -0700

    dialogue bounce fix

--- Senior Thesis/Assets/Scripts/PuzzleScripts/FreeFlowScript.cs
+++ Senior Thesis/Assets/Scripts/PuzzleScripts/FreeFlowScript.cs
@@ -103,204 +103,29 @@ public class FreeFlowScript : MonoBehaviour
     public bool correctcolorred = false;
     public bool correctcolorlilac = false;
 
+
+    public bool done = false;
     public Material currentMaterial;
 
     public Material[] materials;
     public GameObject[] gameobjects;
     //public GameObject[] allgrid;
 
-    GameObject[] orangeTag;
-    GameObject[] pinkTag;
-    GameObject[] darkblueTag;
-    GameObject[] purpleTag;
-    GameObject[] greenTag;
-    GameObject[] tealTag;
-    GameObject[] yellowTag;
-    GameObject[] lightblueTag;
-    GameObject[] redTag;
-    GameObject[] lilacTag;
+    public GameObject[] orangeTag;
+    public GameObject[] pinkTag;
+    public GameObject[] darkblueTag;
+    public GameObject[] purpleTag;
+    public GameObject[] greenTag;
+    public GameObject[] tealTag;
+    public GameObject[] yellowTag;
+    public GameObject[] lightblueTag;
+    public GameObject[] redTag;
+    public GameObject[] lilacTag;
 
-    public void Mouse(GameObject go)
+
+    private void Start()
     {
-        Debug.Log("First part");
-        for (int i = 0; i < gameobjects.Length; i++)
-        {
-            Debug.Log("Second part");
-            if (go == gameobjects[i])
-            {
-                Debug.Log("Third part");
-                currentMaterial = go.GetComponent<MeshRenderer>().material;
-            }
-        }
-        Debug.Log("Fourth part");
-        for (int j = 0; j < materials.Length; j++)
-        {
-            Debug.Log("Fifth part");
-            //if (currentMaterial == materials[j])
-            //{
-                //Debug.Log("Six part");
-                go.GetComponent<MeshRenderer>().material = currentMaterial;
-
-           // }
-        }
-        CheckDine();
-    }
-
-
-    public bool CheckDine()
-    {
-        for(int i = 0; i < orangeTag.Length; i++)
-        {
-            if(orangeTag[i].gameObject.GetComponent<MeshRenderer>().material == materials[4])
-            {
-                correctcolororange = true;
-            }
-            else
-            {
-                correctcolororange = false;
-                break;
-            }
-            break;
-        }
-        for (int i = 0; i < pinkTag.Length; i++)
-        {
-            if (pinkTag[i].gameObject.GetComponent<MeshRenderer>().material == materials[5])
-            {
-                correctcolorpink = true;
-            }
-            else
-            {
-                correctcolorpink = false;
-                break;
-            }
-            break;
-        }
-        for (int i = 0; i < darkblueTag.Length; i++)
-        {
-            if (darkblueTag[i].gameObject.GetComponent<MeshRenderer>().material == materials[0])
-            {
-                correctcolordarkblue = true;
-            }
-            else
-            {
-                correctcolordarkblue = false;
-                break;
-            }
-            break;
-        }
-        for (int i = 0; i < purpleTag.Length; i++)
-        {
-            if (purpleTag[i].gameObject.GetComponent<MeshRenderer>().material == materials[6])
-            {
-                correctcolorpurple = true;
-            }
-            else
-            {
-                correctcolorpurple = false;
-                break;
-            }
-            break;
-        }
-        for (int i = 0; i < greenTag.Length; i++)
-        {
-            if (greenTag[i].gameObject.GetComponent<MeshRenderer>().material == materials[1])
-            {
-                correctcolorgreen = true;
-            }
-            else
-            {
-                correctcolorgreen = false;
-                break;
-            }
-            break;
-        }
-        for (int i = 0; i < tealTag.Length; i++)
-        {
-            if (tealTag[i].gameObject.GetComponent<MeshRenderer>().material == materials[8])
-            {
-                correctcolorteal = true;
-            }
-            else
-            {
-                correctcolorteal = false;
-                break;
-            }
-            break;
-        }
-        for (int i = 0; i < yellowTag.Length; i++)
-        {
-            if (yellowTag[i].gameObject.GetComponent<MeshRenderer>().material == materials[9])
-            {
-                correctcoloryellow = true;
-            }
-            else
-            {
-                correctcoloryellow = false;
-                break;
-            }
-            break;
-        }
-        for (int i = 0; i < lightblueTag.Length; i++)
-        {
-            if (lightblueTag[i].gameObject.GetComponent<MeshRenderer>().material == materials[2])
-            {
-                correctcolorlightblue = true;
-            }
-            else
-            {
-                correctcolorlightblue = false;
-                break;
-            }
-            break;
-        }
-        for (int i = 0; i < redTag.Length; i++)
-        {
-            if (redTag[i].gameObject.GetComponent<MeshRenderer>().material == materials[7])
-            {
-                correctcolorred = true;
-            }
-            else
-            {
-                correctcolorred = false;
-                break;
-            }
-            break;
-        }
-        for (int i = 0; i < lilacTag.Length; i++)
-        {
-            if (lilacTag[i].gameObject.GetComponent<MeshRenderer>().material == materials[3])
-            {
-                correctcolorlilac = true;
-            }
-            else
-            {
-                correctcolorlilac = false;
-                break;
-            }
-            break;
-        }
-
-        if (correctcolororange == true && correctcolorpink == true && correctcolordarkblue == true && correctcolorpurple == true && correctcolorgreen == true && correctcolorteal == true && correctcoloryellow == true && correctcolorlightblue == true && correctcolorred == true && correctcolorlilac == true)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    private void Update()
-    {
-        if (CheckDine() == true)
-        {
-            //Scene transistion
-            GlobalVariables.beatFreeFlow = true;
-            SceneManager.LoadScene("MachineRoom");
-        }
-
-        //do a tag finder to add to array/list of the colors. need to tag each quad piece in unity that needs to be that color with the color name.
-        orangeTag = GameObject.FindGameObjectsWithTag("Orange");
+       /* orangeTag = GameObject.FindGameObjectsWithTag("Orange");
         pinkTag = GameObject.FindGameObjectsWithTag("Pink");
         darkblueTag = GameObject.FindGameObjectsWithTag("DarkBlue");
         purpleTag = GameObject.FindGameObjectsWithTag("Purple");
@@ -310,6 +135,210 @@ public class FreeFlowScript : MonoBehaviour
         lightblueTag = GameObject.FindGameObjectsWithTag("LightBlue");
         redTag = GameObject.FindGameObjectsWithTag("Red");
         lilacTag = GameObject.FindGameObjectsWithTag("Lilac");
-
+        */
     }
+    public void Mouse(GameObject go)
+    {
+        //Debug.Log("First part");
+        for (int i = 0; i < gameobjects.Length; i++)
+        {
+           // Debug.Log("Second part");
+            if (go == gameobjects[i])
+            {
+                //Debug.Log("Third part");
+                currentMaterial = go.GetComponent<MeshRenderer>().sharedMaterial;
+            }
+        }
+        //Debug.Log("Fourth part");
+        for (int j = 0; j < materials.Length; j++)
+        {
+            //Debug.Log("Fifth part");
+            //if (currentMaterial == materials[j])
+            //{
+                //Debug.Log("Six part");
+                go.GetComponent<MeshRenderer>().sharedMaterial = currentMaterial;
+
+           // }
+        }
+        
+    }
+
+
+    public void Update()
+    {
+        for(int k = 0; k < orangeTag.Length - 1; k++)
+        {
+            if(orangeTag[k].gameObject.GetComponent<MeshRenderer>().sharedMaterial == materials[4])
+            {
+                correctcolororange = true;
+                Debug.Log("HereO");
+            }
+            else
+            {
+                correctcolororange = false;
+            }
+
+           // break;
+        }
+        for (int l = 0; l < pinkTag.Length - 1; l++)
+        {
+            if (pinkTag[l].gameObject.GetComponent<MeshRenderer>().sharedMaterial == materials[5])
+            {
+                correctcolorpink = true;
+                Debug.Log("HerePI");
+            }
+            else
+            {
+                correctcolorpink = false;
+            }
+
+            //break;
+        }
+        for (int m = 0; m < darkblueTag.Length - 1; m++)
+        {
+            if (darkblueTag[m].gameObject.GetComponent<MeshRenderer>().sharedMaterial == materials[0])
+            {
+                correctcolordarkblue = true;
+                Debug.Log("HereDB");
+            }
+            else
+            {
+                correctcolordarkblue = false;
+            }
+
+            //  break;
+        }
+        for (int n = 0; n < purpleTag.Length - 1; n++)
+        {
+            if (purpleTag[n].gameObject.GetComponent<MeshRenderer>().sharedMaterial == materials[6])
+            {
+                correctcolorpurple = true;
+                Debug.Log("HereP");
+            }
+            else
+            {
+                correctcolorpurple = false;
+            }
+
+            // break;
+        }
+        for (int o = 0; o < greenTag.Length - 1; o++)
+        {
+            if (greenTag[o].gameObject.GetComponent<MeshRenderer>().sharedMaterial == materials[1])
+            {
+                correctcolorgreen = true;
+                Debug.Log("HereG");
+            }
+            else
+            {
+                correctcolorgreen = false;
+            }
+
+            // break;
+        }
+        for (int p = 0; p < tealTag.Length - 1; p++)
+        {
+            if (tealTag[p].gameObject.GetComponent<MeshRenderer>().sharedMaterial == materials[8])
+            {
+                correctcolorteal = true;
+                Debug.Log("HereT");
+            }
+            else
+            {
+                correctcolorteal = false;
+            }
+
+            // break;
+        }
+        for (int q = 0; q < yellowTag.Length - 1; q++)
+        {
+            if (yellowTag[q].gameObject.GetComponent<MeshRenderer>().sharedMaterial == materials[9])
+            {
+                correctcoloryellow = true;
+                Debug.Log("HereY");
+            }
+            else
+            {
+                correctcoloryellow = false;
+            }
+
+            // break;
+        }
+        for (int r = 0; r < lightblueTag.Length - 1; r++)
+        {
+            if (lightblueTag[r].gameObject.GetComponent<MeshRenderer>().sharedMaterial == materials[2])
+            {
+                correctcolorlightblue = true;
+                Debug.Log("HereLB");
+            }
+            else
+            {
+                correctcolorlightblue = false;
+            }
+
+            // break;
+        }
+        for (int r = 0; r < redTag.Length - 1; r++)
+        {
+            if (redTag[r].gameObject.GetComponent<MeshRenderer>().sharedMaterial == materials[7])
+            {
+                correctcolorred = true;
+                Debug.Log("HereR");
+            }
+            else
+            {
+                correctcolorred = false;
+            }
+
+            // break;
+        }
+        for (int s = 0; s < lilacTag.Length -1; s++)
+        {
+            
+            if (lilacTag[s].gameObject.GetComponent<MeshRenderer>().sharedMaterial == materials[3])
+            {
+                correctcolorlilac = true;
+                Debug.Log("HereL");
+            }
+            else
+            {
+                correctcolorlilac = false;
+            }
+
+            //break;
+        }
+
+        if (correctcolororange == true && correctcolorpink == true && correctcolordarkblue == true && correctcolorpurple == true && correctcolorgreen == true && correctcolorteal == true && correctcoloryellow == true && correctcolorlightblue == true && correctcolorred == true && correctcolorlilac == true)
+        {
+            Debug.Log("Heretrue");
+            done = true;
+        }
+        else
+        {
+            Debug.Log("Hereelse");
+            done = false;
+        }
+
+        if (done == true)
+        {
+            //Scene transistion
+            Debug.Log("Hereupdate");
+            GlobalVariables.beatFreeFlow = true;
+            SceneManager.LoadScene("MachineRoom");
+        }
+
+        //do a tag finder to add to array/list of the colors. need to tag each quad piece in unity that needs to be that color with the color name.
+        /*orangeTag = GameObject.FindGameObjectsWithTag("Orange");
+        pinkTag = GameObject.FindGameObjectsWithTag("Pink");
+        darkblueTag = GameObject.FindGameObjectsWithTag("DarkBlue");
+        purpleTag = GameObject.FindGameObjectsWithTag("Purple");
+        greenTag = GameObject.FindGameObjectsWithTag("Green");
+        tealTag = GameObject.FindGameObjectsWithTag("Teal");
+        yellowTag = GameObject.FindGameObjectsWithTag("Yellow");
+        lightblueTag = GameObject.FindGameObjectsWithTag("LightBlue");
+        redTag = GameObject.FindGameObjectsWithTag("Red");
+        lilacTag = GameObject.FindGameObjectsWithTag("Lilac");
+        */
+    }
+    
 }
