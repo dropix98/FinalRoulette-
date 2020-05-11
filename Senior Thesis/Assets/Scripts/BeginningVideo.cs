4bec7ac969c41443a6971d5fcc59b6f1b80740d6
Justin <bindi@chapman.edu>
2020-05-10 19:24:40 -0700

    main menu play -> loading -> temp video -> floor one final

---
+++ Senior Thesis/Assets/Scripts/BeginningVideo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using System;
+using UnityEngine.SceneManagement;
+
+public class BeginningVideo : MonoBehaviour
+{
+    public GameObject Animatic;
+
+    public double time;
+    public double currentTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+      time = Animatic.GetComponent<VideoPlayer>().clip.length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      checkPlayingVideo();
+
+    }
+
+    private void checkPlayingVideo()
+    {
+      currentTime = Animatic.GetComponent<VideoPlayer>().time;
+       if (currentTime >= time)
+       {
+           Debug.Log ("Video Over");
+           SceneManager.LoadScene("Floor1Final");
+       }
+     }
+}
--- Senior Thesis/Assets/Scripts/LoadSceneMenu.cs
+++ Senior Thesis/Assets/Scripts/LoadSceneMenu.cs
@@ -7,7 +7,7 @@ public class LoadSceneMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Floor1Final");
+        SceneManager.LoadScene("LoadingStart");
     }
 
     public void StartTutorial()
