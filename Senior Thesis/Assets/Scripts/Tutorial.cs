2ec44947f33219eb4a3a3844c5fe667bf09b7e92
Justin <bindi@chapman.edu>
2020-05-14 21:05:44 -0700

    tutorials in

---
+++ Senior Thesis/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+using System;
+
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject Footage;
+    public double time;
+    public double currentTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+      time = Footage.GetComponent<VideoPlayer>().clip.length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      checkPlaying();
+    }
+
+    private void checkPlaying()
+    {
+      Scene currentScene = SceneManager.GetActiveScene();
+      currentTime = Footage.GetComponent<VideoPlayer>().time;
+       if (currentTime >= time)
+       {
+           Debug.Log ("Video Over");
+           if (currentScene.name == "BlockTutorial")
+           {
+             SceneManager.LoadScene("BlockPuzzle");
+           }
+           if (currentScene.name == "SimonSaysTutorial")
+           {
+             SceneManager.LoadScene("SimonSays");
+           }
+       }
+     }
+}
