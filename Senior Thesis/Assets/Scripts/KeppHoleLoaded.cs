33e5e1c23defeac6ed9735318874673bc211297f
kyleburack <42784552+kyleburack@users.noreply.github.com>
2020-05-16 23:33:23 -0700

    hole and YZ deactivating fixed

---
+++ Senior Thesis/Assets/Scripts/KeppHoleLoaded.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeppHoleLoaded : MonoBehaviour
+{
+
+    public GameObject ho;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GlobalVariables.switchingHoleColliders == false)
+        {
+            ho.gameObject.SetActive(true);
+        }
+        else
+        {
+            ho.gameObject.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            GlobalVariables.switchingHoleColliders = true;
+        }
+    }
+}
---
+++ Senior Thesis/Assets/Scripts/KeppYZLoaded.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeppYZLoaded : MonoBehaviour
+{
+    public GameObject yz;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yz.gameObject.SetActive(true);
+    }
+}
